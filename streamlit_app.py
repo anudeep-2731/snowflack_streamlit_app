d307f05abab2b7287c36dd53083ef24e7f6d69ff
anudeep-2731 <58800281+anudeep-2731@users.noreply.github.com>
2024-02-18 16:36:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ streamlit.text("🥣dosa")
 
 myfruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-fruits_selected=streamlit.multiselect("Pick some fruites:",list(myfruit_list.Fruit))
+fruits_selected=streamlit.multiselect("Pick some fruites:",list(myfruit_list.Fruit),['Avocado'])
 
 myfruits_show=myfruit_list.loc[fruits_selected]
 
