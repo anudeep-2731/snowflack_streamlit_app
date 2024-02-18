f0f4fccfb9c55159037b88ad9a25993d8f6ab165
anudeep-2731 <58800281+anudeep-2731@users.noreply.github.com>
2024-02-18 16:21:56 +0530

    Added pandas data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit
+import pandas as pd
 
 streamlit.title("Anudeep Basva")
 
 streamlit.header("Breakfast menu")
 
 streamlit.text("🥣dosa")
+
+myfruit_list=pd.read_csv.("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(myfruit_list)
