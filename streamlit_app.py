ad503d567f85c1ab5ed398748d7bb9181ff055be
anudeep-2731 <58800281+anudeep-2731@users.noreply.github.com>
2024-02-19 14:45:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,11 @@ import streamlit
 import pandas as pd
 import snowflake.connector
 
-streamlit.title("Anudeep Basva")
 
-streamlit.header("Breakfast menu")
-
-streamlit.text("🥣dosa")
-
-myfruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
-myfruit_list=myfruit_list.set_index('Fruit')
-
-fruits_selected=streamlit.multiselect("Pick some fruites:",list(myfruit_list.index),['Avocado'])
-
-myfruits_show=myfruit_list.loc[fruits_selected]
-
-streamlit.dataframe(myfruits_show)
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
+CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
